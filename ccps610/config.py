5f8c97af734899a9ba93c2f747a8caa0142810e8
Jacques D'souza <jacques.x.dsouza@gmail.com>
2018-12-07 02:56:43 +0000

    Initial Commit

--- ccps610/config.py
+++ ccps610/config.py
@@ -4,6 +4,7 @@ import os
 class Config:
     SECRET_KEY = '2367628bb0b13ce0c676dfde280ba245'
     SQLALCHEMY_DATABASE_URI =  os.environ.get('DATABASE_URL')
+    
     MAIL_SERVER = 'smtp.gmail.com'
     #MAIL_PORT = 587
     #MAIL_USE_TLS = True
